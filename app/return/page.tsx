92633dfa11978f49707b6d28a93b48ce0353c6cd
Mandroso22 <goliatherz@gmail.com>
2025-06-04 00:14:59 -0400

    c

--- app/return/page.tsx
+++ app/return/page.tsx
@@ -1,4 +1,8 @@
-import ReturnClient from "./ReturnClient";
+import dynamic from "next/dynamic";
+
+const ReturnClient = dynamic(() => import("./ReturnClient"), {
+  ssr: false,
+});
 
 export default function ReturnPage() {
   return <ReturnClient />;
