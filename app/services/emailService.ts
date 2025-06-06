66f667c22a890a45614c4b5b219c129c4169513e
Mandroso22 <goliatherz@gmail.com>
2025-06-06 13:30:43 -0400

    commit

--- app/services/emailService.ts
+++ app/services/emailService.ts
@@ -15,7 +15,9 @@ interface CustomerInfo {
 }
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // true pour le port 465, false pour les autres ports
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
