eda49574e8e03f5dc3575ff312933f49ff858989
Mandroso22 <goliatherz@gmail.com>
2025-06-07 22:11:56 -0400

    commit

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,4 +1,8 @@
-import SuccessClient from "../success/SuccessClient";
+import dynamic from "next/dynamic";
+
+const SuccessClient = dynamic(() => import("./SuccessClient"), {
+  ssr: false,
+});
 
 export default function SuccessPage() {
   return <SuccessClient />;
